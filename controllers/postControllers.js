6bcb575fc3eb7b7f905c134f0b90eb589a0a43dd
Arlen Freitas <arlenfreitas1@gmail.com>
2021-10-14 19:01:05 -0300

    req 08

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -1,10 +1,16 @@
 const postService = require('../services/postServices');
+const { BlogPost, User, Category } = require('../models');
 
 const createPost = async (req, res) => {
   const post = await postService.createPost(req.body, req.user.id);
   res.status(201).json(post);
 };
-
+const getAllPosts = (_req, res) => BlogPost.findAll({
+  include: [
+    { model: User, as: 'user' },
+    { model: Category, as: 'categories', through: { attributes: [] } },
+  ] }).then((data) => res.status(200).json(data));
 module.exports = {
   createPost,
+  getAllPosts,
 };
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const rescue = require('express-rescue');
-const { createPost } = require('../controllers/postControllers');
+const { createPost, getAllPosts } = require('../controllers/postControllers');
 const { validatePost } = require('../middlewares/postValidators');
 const { validateToken } = require('../middlewares/tokenValidators');
 
 const postRoute = express.Router();
 
 postRoute.post('/', validatePost, validateToken, rescue(createPost));
+postRoute.get('/', validateToken, rescue(getAllPosts));
 
 module.exports = postRoute;
